014750f2f19bf3fbedf5c5808ff01b8263b853ac
John-Philip Taylor <John-PhilipT@reutech.co.za>
2018-08-10 09:31:53 +0200

    Modified the license to the Mozilla Public License Version 2.0

--- Test/testDebugMessages.cpp
+++ Test/testDebugMessages.cpp
@@ -1,3 +1,14 @@
+//==============================================================================
+// Copyright (C) John-Philip Taylor
+// jpt13653903@gmail.com
+//
+// This file is part of a library
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//==============================================================================
+
 #include "test.h"
 //------------------------------------------------------------------------------
 
